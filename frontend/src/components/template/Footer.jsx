599decd8efd6562b629b9cfc80bf55a25c3dd88e
ErnaniUlsenheimer <ernaniu@gmail.com>
2022-06-19 13:40:00 -0300

    Site CRUD React

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido por
+            <strong> Ernani Ulsenheimer</strong>
+        </span>
+    </footer>
